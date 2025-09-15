e1e539c1bac55cdea15aed28ddeae5bd0ed2e9e0
Muhammad Ahmad Riaz <200363561+mirzaahmad007@users.noreply.github.com>
2025-09-15 10:52:40 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'animates2.dart';
 import 'containeranimated.dart';
 
 void main() {
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: AniContainer()
+      home:Animates2()
     );
   }
 }
