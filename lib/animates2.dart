f9bea772a47141cae89f2652c6378db375d9351e
Muhammad Ahmad Riaz <200363561+mirzaahmad007@users.noreply.github.com>
2025-09-16 10:31:55 +0500

    assets

--- lib/animates2.dart
+++ lib/animates2.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Animates2 extends StatefulWidget {
+
   const Animates2({super.key});
 
   @override
@@ -9,27 +11,76 @@ class Animates2 extends StatefulWidget {
 
 class _Animates2State extends State<Animates2>
 with SingleTickerProviderStateMixin{
+  int count=0;
 
   bool like=false;
 
-  late AnimationController _controller;
 
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    _controller= AnimationController(
-        vsync: this,
-      duration: Duration(seconds: 2)
-    )..repeat();
-    super.initState();
-  }
+void increament(){
+  setState(() {
+    count++;
+  });
+}
+void decreament(){
+  setState(() {
+   if(count>0){
+     count--;
+   }
+  });
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        children: [
+          GestureDetector(
+            onTap: (){
+              setState(() {
+                like=!like;
+              });
+            },
+            child: Center(
+              child: Container(
+                margin: EdgeInsets.only(top: 200),
+                height: 50,
+                width: 70,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                  borderRadius: BorderRadius.all(Radius.circular(15))
+                ),
+            child: like? Icon(Icons.favorite_outline): Icon(Icons.favorite, color: Colors.red,),
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Container(
+            width: MediaQuery.of(context).size.width*0.4,
+            height: MediaQuery.of(context).size.height*0.06,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              color: Colors.blue.shade300
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              
+              children: [
+                GestureDetector(
+                  onTap:increament,
+                    child: Icon(Icons.add, size: 25,)),
+                GestureDetector(
+                  onTap: (){
 
-      body: Container(
-        child: like? Icon(Icons.favorite_outline): Icon(Icons.favorite),
+                  },
+                    child: Text("$count", style: GoogleFonts.nerkoOne(fontSize: 24),)),
+                GestureDetector(
+                  onTap: decreament,
+                    child: Icon(Icons.remove, size: 25,))
+
+              ],
+
+            ),
+          )
+        ],
       ),
     );
   }
